90fcd33abdd6e65948feddc9a45929f8cb0e6670
Surendra Verma <suver@microsoft.com>
2016-07-20 10:12:30 +0530

    Installed express js

--- Server/web_server.js
+++ Server/web_server.js
@@ -1,17 +1,19 @@
-var http = require("http");
-var server = http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write("<!DOCTYPE 'html'>");
-  response.write("<html>");
-  response.write("<head>");
-  response.write("<title>Wishlist web server</title>");
-  response.write("</head>");
-  response.write("<body>");
-  response.write("This is SK's first web service!");
-  response.write("</body>");
-  response.write("</html>");
-  response.end();
-});
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-server.listen(1000);
-console.log("Server is listening");
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
