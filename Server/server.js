1e4a7e0cee86c7936cc15670cc59aebac8baa90e
Surendra Verma <suver@microsoft.com>
2016-07-24 16:22:20 +0530

    test commit

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,22 @@
 var express    = require("express");
 
 var app = express();
-/*
+
+
+app.get('/', function (req, res) {
+   
+res.end("<html><head><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script></head><title>Wishlist</title><body><p>Welcome</p><a href='/list_movies'>List of Movies</a><br/><a href='/list_actors'>List of Actors</a><br/><a href='/list_producers'>List of Producers</a><br/><a href='/list_directors'>List of Directors</a><br/><a href='/list_music_directors'>List of Music Directors</a><br/><a href='/list_productions'>List of Productions</a><br/><br/><input type='text'  onchange='searchByTitle(this)' placeholder='Search By Movie Title'><br/><br/><input type='text'  onchange='searchByActor(this)' placeholder='Search By Actor'><br/><br/><input type='text'  onchange='searchByDirector(this)' placeholder='Search By Director'><br/><br/><input type='text'  onchange='searchByProducer(this)' placeholder='Search By Producer'><br/><br/><input type='text'  onchange='searchByMusicDirector(this)' placeholder='Search By Music Director'><br/><br/><input type='text'  onchange='searchByProductionHouse(this)' placeholder='Search By Production House'><br/><br/><p id='search_output'>result</p><script>function searchByTitle(obj){ showResult ('search_by_title', obj.value);}function searchByActor(obj){ showResult ('search_by_actor', obj.value);}function searchByDirector(obj){ showResult ('search_by_director', obj.value);}function searchByProducer(obj){ showResult ('search_by_producer', obj.value);}function searchByMusicDirector(obj){ showResult ('search_by_music_director', obj.value);}function searchByProductionHouse(obj){ showResult ('search_by_production_house', obj.value);}function showResult( searchType, searchString){  var searchUrl='/'+searchType+'?search_query='+ searchString; $.ajax({type: 'GET', url: searchUrl, success:function(result){ $('#search_output').html(result); }});}</script></body></html>");
+})
+
+var port = process.env.PORT || 8080;
+
+var server = app.listen(port, function () {
+
+  console.log("Wishlist app listening at http://localhost:%s", port)
+
+})
+
+
 var $ = require('jQuery');
 var handleRequest = require('supertest');
 var fs = require("fs");
@@ -24,13 +39,9 @@ connection.connect(function(err){
   }
 });
 
-*/
-app.get('/', function (req, res) {
-   
-res.end("<html><head><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'></script></head><title>Wishlist</title><body><p>Welcome</p><a href='/list_movies'>List of Movies</a><br/><a href='/list_actors'>List of Actors</a><br/><a href='/list_producers'>List of Producers</a><br/><a href='/list_directors'>List of Directors</a><br/><a href='/list_music_directors'>List of Music Directors</a><br/><a href='/list_productions'>List of Productions</a><br/><br/><input type='text'  onchange='searchByTitle(this)' placeholder='Search By Movie Title'><br/><br/><input type='text'  onchange='searchByActor(this)' placeholder='Search By Actor'><br/><br/><input type='text'  onchange='searchByDirector(this)' placeholder='Search By Director'><br/><br/><input type='text'  onchange='searchByProducer(this)' placeholder='Search By Producer'><br/><br/><input type='text'  onchange='searchByMusicDirector(this)' placeholder='Search By Music Director'><br/><br/><input type='text'  onchange='searchByProductionHouse(this)' placeholder='Search By Production House'><br/><br/><p id='search_output'>result</p><script>function searchByTitle(obj){ showResult ('search_by_title', obj.value);}function searchByActor(obj){ showResult ('search_by_actor', obj.value);}function searchByDirector(obj){ showResult ('search_by_director', obj.value);}function searchByProducer(obj){ showResult ('search_by_producer', obj.value);}function searchByMusicDirector(obj){ showResult ('search_by_music_director', obj.value);}function searchByProductionHouse(obj){ showResult ('search_by_production_house', obj.value);}function showResult( searchType, searchString){  var searchUrl='/'+searchType+'?search_query='+ searchString; $.ajax({type: 'GET', url: searchUrl, success:function(result){ $('#search_output').html(result); }});}</script></body></html>");
-})
 
-/*
+
+
 app.get('/list_movies', function (req, res) {
    
     var queryString= "select moviescollection.title from moviescollection";
@@ -184,12 +195,3 @@ $.urlParam = function(name , url){
   return results[1] || 0;
 }
 
-*/
-
-var port = process.env.PORT || 8080;
-
-var server = app.listen(port, function () {
-
-  console.log("Wishlist app listening at http://localhost:%s", port)
-
-})
