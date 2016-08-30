7d2d6e5cd83a63c4ede4b53b4a67a3b7ed0352bf
Surendra Verma <suver@microsoft.com>
2016-08-30 11:51:41 +0530

    git ignore

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+var LOCALBASEURL = "http://localhost:8080/"; 
+var TESTBASEURL = "https://wishlist-testserverapp.herokuapp.com/";
+var PRODBASEURL = "https://wishlist-serverapp.herokuapp.com/";
+ 
+//export default LOCALBASEURL;
+//export default TESTBASEURL;
+export default PRODBASEURL;
