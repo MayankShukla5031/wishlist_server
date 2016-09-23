1e1d354e581046071c78bdd2eca4f89a2ecb3849
SK Verma <titanic.love.sk@gmail.com>
2016-09-23 22:02:08 +0530

    expose headers

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(session({
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Expose-Headers", "*");
   next();
 });
 
