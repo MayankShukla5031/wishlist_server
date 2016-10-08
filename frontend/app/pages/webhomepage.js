9471258dd26e0c0092de1338b54addd6390e09c8
S K Verma <titanic.love.sk@gmail.com>
2016-10-08 12:48:23 +0530

    ui login fix

--- frontend/app/pages/webhomepage.js
+++ frontend/app/pages/webhomepage.js
@@ -393,14 +393,14 @@ export default class HomePage extends React.Component {
                                     <MenuItem key={6} value="production house" primaryText="Production House" />                                  
                                 </SelectField>
 
-                                <ul style={{listStyle: "none", marginTop: 'auto', cursor: 'pointer', height: '48px', textAlign: 'center'}} onClick={this._openUserOption.bind(this)}>
+                                <ul style={{listStyle: "none", marginTop: '20px', cursor: 'pointer', height: '48px', textAlign: 'center'}} onClick={this._openUserOption.bind(this)}>
                                     <li>
                                         <Avatar
-                                            src={this.state.isLoggedin ? this.state.userTypeValue == "user" ? "user.png" : "theatre.jpg" : ""}
+                                            src={this.state.isLoggedin ? this.state.userTypeValue == "user" ? "user.png" : "theatre.jpg" : "login.png"}
                                         >
                                         </Avatar>
                                     </li>
-                                    {Api._getKey("username") ? <li style={{fontSize: '10px'}}>{Api._getKey("username")}</li> : ""}
+                                    {Api._getKey("username") ? <li style={{fontSize: '10px'}}>{Api._getKey("username")}</li> : "Login"}
                                 </ul>
                                 <Popover
                                     open={this.state.openUserOption}
