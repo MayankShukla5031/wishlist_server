8c9f3f72f9d9dfd6de6dacb5e9aeb52aa9ee2590
SK Verma <titanic.love.sk@gmail.com>
2016-10-08 23:33:06 +0530

    remove posters

--- server.js
+++ server.js
@@ -601,30 +601,6 @@ app.get('/:action', function (req, res)
           res.end(JSON.stringify(list));
                      });      
     }
-    else if(action== "poster_small")
-    {
-      fs.readFile('posters/small/'+ req.query.movieid + '.jpg', function(err, contents) {
-
-          if (err) console.log(err);
-          else 
-            {              
-              res.write(contents);
-              res.end();
-          }
-        }); 
-    }
-    else if(action== "poster_big")
-    {
-      fs.readFile('posters/big/'+ req.query.movieid + '.jpg', function(err, contents) {
-
-          if (err) console.log(err);
-          else 
-            {              
-              res.write(contents);
-              res.end();
-          }
-        }); 
-    }
     else
     {
     	 res.end("unknown request" );
