5a10a8b503e31fa31ab95919aa2785f303301a54
Naveen <naveen.chittipolu@gamechangesns.com>
2016-11-02 22:58:28 +0400

    Updated development config - Added mongo cloud instance URI

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,8 @@
 module.exports = {
     // MongoDB connection options
     mongo: {
-         uri: 'mongodb://localhost/movwish-dev'
+         //uri: 'mongodb://localhost/movwish-dev'
+        uri: 'mongodb://movwishAdmin:movwishAdmin@ds031852.mlab.com:31852/movwish'
     },
     host:'localhost',
     cluster:false
