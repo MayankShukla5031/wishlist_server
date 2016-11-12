6b175820c5a425143783f0adc8b5baee2107341a
Naveen <naveen.chittipolu@gamechangesns.com>
2016-11-12 23:09:56 +0400

    Config Development JS pointing to cloud instance

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,8 @@
 module.exports = {
     // MongoDB connection options
     mongo: {
-         //uri: 'mongodb://localhost/movwish-dev'
-        uri: 'mongodb://movwishAdmin:movwishAdmin@ds031852.mlab.com:31852/movwish'
+          // uri: 'mongodb://localhost/movwish-cloud'
+         uri: 'mongodb://movwishAdmin:movwishAdmin@ds031852.mlab.com:31852/movwish'
     },
     host:'localhost',
     cluster:false
